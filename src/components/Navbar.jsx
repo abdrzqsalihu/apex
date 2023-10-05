2ffde0acb6365978b5d55640e48c8f929b798dcf
Abdulrazaq Salihu <abdrzq.salihu@gmail.com>
2023-10-05 12:34:26 +0100

    added background-blur to the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ function Navbar() {
   };
 
   return (
-    <header className='fixed top-0 left-0 bg-white w-full z-50 px-5'>
+    <header className='fixed top-0 left-0 bg-white w-full z-50 px-5 backdrop-blur-lg bg-opacity-70 shadow-sm'>
       <nav className="container mx-auto flex items-center justify-between py-4">
         <div className="container mx-auto lg:w-[1296px] lg:h-[50px] flex justify-between items-center">
           {/* Logo on the left */}
